e9668e884093e582d784eeaa9ed0286f854bc0b1
Ali Ezadkhaha <hodhod212@yahoo.com>
2019-10-23 15:04:07 +0200

    Updates

---
+++ precache-manifest.5cfc2f4d0489fc285027b19ea30e76b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17eb721303d395a47ea0579d77d6c333",
+    "url": "/monster-Rolodex/index.html"
+  },
+  {
+    "revision": "4e7bddfe303c5778c43e",
+    "url": "/monster-Rolodex/static/css/main.bb504fd7.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monster-Rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "4e7bddfe303c5778c43e",
+    "url": "/monster-Rolodex/static/js/main.510a0102.chunk.js"
+  },
+  {
+    "revision": "56e9886dde78ce7b6857",
+    "url": "/monster-Rolodex/static/js/runtime-main.11cd94ab.js"
+  }
+]);
